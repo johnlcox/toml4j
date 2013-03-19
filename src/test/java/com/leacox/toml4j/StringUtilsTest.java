1283835d129d202a651b64464292a3173cb7952b
John Leacox <johnlcox@gmail.com>
2013-03-18 21:01:28 -0500

    Add StringUtils test for unicode control character

--- src/test/java/com/leacox/toml4j/StringUtilsTest.java
+++ src/test/java/com/leacox/toml4j/StringUtilsTest.java
@@ -6,11 +6,18 @@ import org.junit.Test;
 
 public class StringUtilsTest {
 	@Test
-	public void testunescapeStringBasicUnicode() {
-		String value = "\u00e9";
+	public void testUnescapeStringBasicUnicode() {
+		String value = "\\u00e9";
 
 		String result = StringUtils.unescapeString(value);
 
 		assertEquals("é", result);
+	}
+
+	@Test(expected = ParseException.class)
+	public void testUnescapeStringThrowsParseExceptionFOrUnicodeControlCharacter() {
+		String value = "\\u0005";
+
+		StringUtils.unescapeString(value);
 	}
 }
